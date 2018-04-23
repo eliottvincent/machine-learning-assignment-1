ac12c8a7fa713f425af068407a7dc95af7627860
Eliott Vincent <eliott.20100@gmail.com>
2018-04-23 21:11:16 +0100

    gitflow-feature-stash: distinction / added new lists

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 import pandas as pd
 
+continuousFeatures = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-gain', 'hours-per-week']
+categoricalFeatures = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country']
+continuousMeasures = ['feature_name', 'count', 'miss_percentage', 'card', 'min', 'first_quartile', 'mean', 'median', 'third_quartile', 'max', 'std_dev'];
+categoricalMeasures = ['feature_name', 'count', 'miss_percentage', 'card', 'mode', 'mode_frequency', 'mode_percentage', 'second_mode', 'second_mode_frequency', 'second_mode_percentage'];
+
 # main
 def main():
 	df = load_dataframe('./data/DataSet.csv')
 	print(df)
 	write_dataframe('./data/Test.csv',df)
 
-
-# load_DataFrame
+# load_dataFrame
 def load_dataframe(path):
-	return pd.read_csv(path, header=0, index_col='id')
+	return pd.read_csv(path, header=0, index_col='id', na_values=['?'])
 
 # write_dataframe
 def write_dataframe(path,dataframe):
