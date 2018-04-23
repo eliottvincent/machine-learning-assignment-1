7d687ff7ecb66155737a2ec64a6d262426e94ce9
Eliott Vincent <eliott.20100@gmail.com>
2018-04-23 22:44:52 +0100

    splitted dataframes

--- main.py
+++ main.py
@@ -1,16 +1,16 @@
 import pandas as pd
 
-continuousFeatures = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-gain', 'hours-per-week']
-categoricalFeatures = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country']
+continuousFeatures = ['age', 'fnlwgt', 'education-num', 'capital-gain', 'capital-loss', 'hours-per-week']
+categoricalFeatures = ['workclass', 'education', 'marital-status', 'occupation', 'relationship', 'race', 'sex', 'native-country', 'target']
 continuousMeasures = ['feature_name', 'count', 'miss_percentage', 'card', 'min', 'first_quartile', 'mean', 'median', 'third_quartile', 'max', 'std_dev']
 categoricalMeasures = ['feature_name', 'count', 'miss_percentage', 'card', 'mode', 'mode_frequency', 'mode_percentage', 'second_mode', 'second_mode_frequency', 'second_mode_percentage']
 
 # main
 def main():
 	df = load_dataframe('./data/DataSet.csv')
-	print(df)
+	# print(df)
 	write_dataframe('./data/Test.csv',df)
-	generateContinuousReport(df)
+	generateCategoricalReport(df)
 
 # load_dataFrame
 def load_dataframe(path):
@@ -24,31 +24,37 @@ def write_dataframe(path,dataframe):
 def generateContinuousReport(df):
 	
 	# 1st step: create a new dataframe from continuousFeatures
-
-	# 2nd step: create an empty dataframe for the continuous measures
-	continuousDf = pd.DataFrame.from_records([], continuousMeasures)
+	continuousDf = df.drop(categoricalFeatures, axis=1)
 
 	print(continuousDf)
+
+	# 2nd step: create an empty dataframe for the continuous measures
+	measuresDf = pd.DataFrame.from_records([], continuousMeasures)
+
+	# print(measuresDf)
 	# 3rd step: loop over the new dataframe
 		# computing each measure (count, miss, card, etc.) (with Arthur's methods)
 		# adding the row to the global result dataframe
-		
+
 
 	# 4th step: return the final dataframe
-	return continuousDf
+	return measuresDf
 
 
 
 def generateCategoricalReport(df):
 	# 1st step: create a new dataframe from categoricalFeatures
+	categoricalDf = df.drop(continuousFeatures, axis=1)
+
+	print(categoricalDf)
 
 	# 2nd step: create a dataframe for the categorical measures
-	categoricalDf = pd.DataFrame.from_records([], categoricalMeasures)
+	measuresDf = pd.DataFrame.from_records([], categoricalMeasures)
 
 	# 3rd step: loop over the new dataframe
 		# computing each measure (count, miss, card, etc.) (with Arthur's methods)
 		# adding the row to the global result dataframe
 
 	# 4th step: return the final dataframe
-	return df
+	return measuresDf
 main()
