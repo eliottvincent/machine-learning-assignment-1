d219d5db0c3ab168170228fad11c6be9abaf684c
Arthur Chevallier <arthur.chevallier22@gmail.com>
2018-04-23 19:22:46 +0100

    Auto stash before merge of "master" and "origin/master"

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import pandas as pd
 def main():
 	df = load_dataframe('./data/DataSet.csv')
 	print(df)
+	write_dataframe('./data/Test.csv',df)
 
 
 # load_DataFrame
 def load_dataframe(path):
-	return pd.read_csv(path, header=None)
+	return pd.read_csv(path, header=0, index_col='id')
 
+# write_dataframe
+def write_dataframe(path,dataframe):
+	dataframe.to_csv(path)
 
 # num_missing
 def num_missing(x):
